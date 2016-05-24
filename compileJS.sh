76e16a383f7d413a467779f13f6031068c549690
Carsten Blüm <carsten@bluem.net>
2016-05-24 20:16:26 +0200

    Create libraries dir if needed, fix name in Readme

--- compileJS.sh
+++ compileJS.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -e "$HOME/Library/Script Libraries" ]
+then
+	mkdir "$HOME/Library/Script Libraries"
+fi
+
+if [ ! -d "$HOME/Library/Script Libraries" ]
+then
+	>&2 echo "Not a directory: $HOME/Library/Script Libraries"
+	exit 1
+fi
+
 osacompile -l JavaScript -o "$HOME/Library/Script Libraries/PashuaJS.scpt"  "$DIR/PashuaJS.applescript"
 osacompile -l JavaScript -o "Example1ConfigTextJS.scpt" "$DIR/Example1ConfigTextJS.applescript"
 osacompile -l JavaScript -o "Example2ConfigObjectJS.scpt" "$DIR/Example2ConfigObjectJS.applescript"
